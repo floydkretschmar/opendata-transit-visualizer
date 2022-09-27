0276d216098f7f8336ea214ce506b1282edb9ef7
Floyd Kretschmar <fkretschmar@googlemail.com>
2022-09-27 13:11:46 +0200

    Initial commit

---
+++ src/main/kotlin/de/floydkretschmar/opendatatransitvisualizer/OpenDataTransitVisualizerApplication.kt
@@ -0,0 +1,11 @@
+package de.floydkretschmar.opendatatransitvisualizer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenDataTransitVisualizerApplication
+
+fun main(args: Array<String>) {
+    runApplication<OpenDataTransitVisualizerApplication>(*args)
+}
